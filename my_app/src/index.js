6f6c02163097a8f9ba269af9cc91c6c46d49f263
Roman K <romichsh@outlook.com>
2019-06-06 02:02:08 +0300

    myApp -1.

--- my_app/src/index.js
+++ my_app/src/index.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+ let helloWord = <h1>Hello word</h1>;
+ class HelloWord extends React.Component{
+  render(){
+   //console.log(Object.isFrozen(this.props));
+   //  if (this.props.heading)  return React.createElement('h1', null, 'Hello') else return React.createElement('p', null, 'Hello')
+   return (
+          <div>
+              {helloWord}
+              {helloWord}
+          </div>
+         )
+  }
+ }
+/////////////////
+ class DateTime extends React.Component{
+     render(){
+         let dateTimeNow = new Date().toLocaleString()
+         return <span>Current date and time {dateTimeNow}</span>
+     }
+ }
+ReactDOM.render(<HelloWord/>
+              , document.getElementById('content'));
+
